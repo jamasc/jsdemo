c268e9528162e15c04535164598dad22153ef81a
Mark <56241393+jamasc@users.noreply.github.com>
2024-04-01 13:06:26 -1000

    updated

--- script.js
+++ script.js
@@ -1,31 +1,8 @@
-class ValueDisplay {
-  // Constructor to initialize properties
-  constructor(...args) {
-    this.args = args;
-  }
 
-  displayProperties() {
-    document.getElementById("display").innerHTML = `
-      <p>Property 1: ${this.property1}</p>
-      <p>Property 2: ${this.property2}</p>
-    `;
-  }
-  
-  // Method to execute when div is clicked
-  doSomething() {
-    console.log("Div clicked! Do something...");
-  }
-}
+const oldInfo = 5;
 
-// Create an instance of the class
-const myInstance = new MyClass("Value 1", "Value 2");
-
-// Call the method to display properties when the page loads
-window.onload = function() {
-  myInstance.displayProperties();
-  
-  // Add click event listener to the div
-  document.getElementById("display").addEventListener("click", function() {
-    myInstance.doSomething();
-  });
-};
+const allInfo = `
+	<p>old Info: ${oldInfo}</p>
+	`;
+	
+document.getElementById("display").innerHTML = allInfo;
